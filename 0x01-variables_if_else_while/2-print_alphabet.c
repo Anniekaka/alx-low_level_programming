249e7d029673e5f52b7ff2ab0641da49e6affcbb
Anniekaka <zinnyamy@gmail.com>
2022-12-17 21:56:49 -0800

    Print alphabe

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'a');
+	{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
